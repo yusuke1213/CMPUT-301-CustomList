6740bb7d27f735ffdca4e5a2017050ec16f73aed
yusuke_yamashiro <yusuke.y.nz1213@gmail.com>
2022-03-09 10:04:31 -0700

    test case is failing for addCity

---
+++ app/src/test/java/CustomListTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.assertEquals;
+
+import com.example.simpleparadox.listycity.City;
+import com.example.simpleparadox.listycity.CustomList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class CustomListTest {
+    private CustomList list;
+
+    @Before
+    public void createList(){
+        list = new CustomList(null, new ArrayList<City>());
+    }
+
+    @Test
+    public void addCityTest(){
+        int listSize = list.getCount(); // give value 4
+        list.addCity(new City("Halifax", "NS"));
+        assertEquals(listSize+1, list.getCount()); //get the value 5
+    }
+
+
+
+
+}
